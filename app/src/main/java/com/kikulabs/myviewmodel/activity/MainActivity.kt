f93d787a262952809760affd5f561b2a80db1682
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-04-29 08:08:13 +0700

    Implemented viewmodel

---
+++ app/src/main/java/com/kikulabs/myviewmodel/activity/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.kikulabs.myviewmodel.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.kikulabs.myviewmodel.R
+import com.kikulabs.myviewmodel.databinding.ActivityMainBinding
+import com.kikulabs.myviewmodel.viewmodel.MainViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        displayResult()
+
+        binding.btnCalculate.setOnClickListener {
+            val width = binding.edtWidth.text.toString()
+            val height = binding.edtHeight.text.toString()
+            val length = binding.edtLength.text.toString()
+
+            when {
+                width.isEmpty() -> {
+                    binding.edtWidth.error = "Masih kosong"
+                }
+                height.isEmpty() -> {
+                    binding.edtHeight.error = "Masih kosong"
+                }
+                length.isEmpty() -> {
+                    binding.edtLength.error = "Masih kosong"
+                }
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+        }
+    }
+
+    private fun displayResult() {
+        binding.tvResult.text = viewModel.result.toString()
+    }
+}
